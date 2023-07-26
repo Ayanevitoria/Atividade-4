f7b02bd06e367f65d42ee8f74199c8771ee21332
Ayanevitoria <108302886+Ayanevitoria@users.noreply.github.com>
2023-07-26 19:45:52 +0000

    código flask com js

--- static/js/questao04.js
+++ static/js/questao04.js
@@ -1,16 +1,17 @@
 function alterarImagem(){
     var forma = document.getElementById("forma").value
     if (forma === "círculo"){
-      document.getElementById('img').src = "circulo.jpg";
+      document.getElementById('img').src="{{ url_for('static', filename='img/circulo.jpg') }}";
     }
     else if (forma === "quadrado"){
-      document.getElementById('img').src = "quadrado.jpg";
+      document.getElementById('img').src="{{ url_for('static', filename='img/quadrado.jpg') }}";
     }
     else if (forma === "triângulo"){
-      document.getElementById('img').src = "triangulo.jpg";
+      document.getElementById('img').src="{{ url_for('static', filename='img/triangulo.jpg') }}";
     }
     else{
-      document.getElementById('img').src = "interrogacao.jpg";
+      document.getElementById('img').src="{{ url_for('static', filename='img/interrogacao.jpg') }}";
       window.alert("Forma geométrica inválida! Escolha uma das formas geométricas (círculo, quadrado ou triângulo).")
     }
   }
+  
