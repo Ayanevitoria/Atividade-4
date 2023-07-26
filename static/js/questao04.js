267506972566f65a4af1c080e574929b90e7e2bc
Ayanevitoria <108302886+Ayanevitoria@users.noreply.github.com>
2023-07-26 19:55:01 +0000

    código flask com js da atividade

--- static/js/questao04.js
+++ static/js/questao04.js
@@ -1,16 +1,16 @@
 function alterarImagem(){
     var forma = document.getElementById("forma").value
     if (forma === "círculo"){
-      document.getElementById('img').src="{{ url_for('static', filename='img/circulo.jpg') }}";
+      document.getElementById('img').src='static/img/circulo.jpg';
     }
     else if (forma === "quadrado"){
-      document.getElementById('img').src="{{ url_for('static', filename='img/quadrado.jpg') }}";
+      document.getElementById('img').src='static/img/quadrado.jpg';
     }
     else if (forma === "triângulo"){
-      document.getElementById('img').src="{{ url_for('static', filename='img/triangulo.jpg') }}";
+      document.getElementById('img').src='static/img/triangulo.jpg';
     }
     else{
-      document.getElementById('img').src="{{ url_for('static', filename='img/interrogacao.jpg') }}";
+      document.getElementById('img').src='static/img/interrogacao.jpg';
       window.alert("Forma geométrica inválida! Escolha uma das formas geométricas (círculo, quadrado ou triângulo).")
     }
   }
--- static/js/questao05.js
+++ static/js/questao05.js
@@ -1,3 +1,3 @@
 var flores = ["begonia", "dalia", "flordecerejeira","girassol", "hortencia", "lavanda", "lirio", "magnolia", "tulipa", "violeta"]
 for (f=0; f<flores.length; f++)
-  document.getElementById('flores').innerHTML += "<img src='"+flores[f]+".jpg' alt=''>"
+  document.getElementById('flores').innerHTML += "<img src='static/img/"+flores[f]+".jpg' alt=''>"
